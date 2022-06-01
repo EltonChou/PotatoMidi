851e2aef3051d1ce0f07dd61cf61363792f11d8d
Elton Chou <plscd748@gmail.com>
2022-06-01 14:45:45 +0800

    change default  pin define

--- src/potato_midi_defs.h
+++ src/potato_midi_defs.h
@@ -4,34 +4,34 @@
 #include <Arduino.h>
 
 #ifndef HC165_LOAD_PIN
-#define HC165_LOAD_PIN 2
+#define HC165_LOAD_PIN 9
 #endif
 
 #ifndef HC165_CLOCK_PIN
-#define HC165_CLOCK_PIN 3
+#define HC165_CLOCK_PIN 7
 #endif
 
 #ifndef HC165_LATCH_PIN
-#define HC165_LATCH_PIN 4
+#define HC165_LATCH_PIN 8
 #endif
 
 #ifndef HC165_SERIAL_DATA_IN_PIN
-#define HC165_SERIAL_DATA_IN_PIN 5
+#define HC165_SERIAL_DATA_IN_PIN 6
 #endif
 
 #define HC165_CHAIN_COUNT 2
 #define HC165_DATA_LENGTH 8 * HC165_CHAIN_COUNT
 
 #ifndef HC595_LATCH_PIN
-#define HC595_LATCH_PIN 6
+#define HC595_LATCH_PIN 10
 #endif
 
 #ifndef HC595_CLOCK_PIN
-#define HC595_CLOCK_PIN 7
+#define HC595_CLOCK_PIN 11
 #endif
 
 #ifndef HC595_SERIAL_DATA_OUT_PIN
-#define HC595_SERIAL_DATA_OUT_PIN 8
+#define HC595_SERIAL_DATA_OUT_PIN 12
 #endif
 
 #define HC595_CHAIN_COUNT 2
@@ -48,19 +48,19 @@
 #endif
 
 #ifndef HC4067_MUX_S0_PIN
-#define HC4067_MUX_S0_PIN 9
+#define HC4067_MUX_S0_PIN 2
 #endif
 
 #ifndef HC4067_MUX_S1_PIN
-#define HC4067_MUX_S1_PIN 10
+#define HC4067_MUX_S1_PIN 3
 #endif
 
 #ifndef HC4067_MUX_S2_PIN
-#define HC4067_MUX_S2_PIN 11
+#define HC4067_MUX_S2_PIN 4
 #endif
 
 #ifndef HC4067_MUX_S3_PIN
-#define HC4067_MUX_S3_PIN 12
+#define HC4067_MUX_S3_PIN 5
 #endif
 
 #define CONTROL_ON 127
