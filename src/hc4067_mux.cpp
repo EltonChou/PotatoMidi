ea300f3dc2856a78968018ebb537a0c657fc61c2
Elton Chou <plscd748@gmail.com>
2022-06-02 17:56:26 +0800

    declare variable in cpp

--- src/hc4067_mux.cpp
+++ src/hc4067_mux.cpp
@@ -1,8 +1,33 @@
 #include "hc4067_mux.h"
 
+const int s_pins[4] = {
+    HC4067_MUX_S0_PIN,
+    HC4067_MUX_S1_PIN,
+    HC4067_MUX_S2_PIN,
+    HC4067_MUX_S3_PIN};
+
+const int mux_channel[16][4] = {
+    // {S0, S1, S2, S3,}
+    {0, 0, 0, 0},
+    {1, 0, 0, 0},
+    {0, 1, 0, 0},
+    {1, 1, 0, 0},
+    {0, 0, 1, 0},
+    {1, 0, 1, 0},
+    {0, 1, 1, 0},
+    {1, 1, 1, 0},
+    {0, 0, 0, 1},
+    {1, 0, 0, 1},
+    {0, 1, 0, 1},
+    {1, 1, 0, 1},
+    {0, 0, 1, 1},
+    {1, 0, 1, 1},
+    {0, 1, 1, 1},
+    {1, 1, 1, 1}};
+
 int hc4067_mux::read_channel_value(int channel)
 {
-    for (size_t i; i < sizeof(s_pins); i++)
+    for (int i; i < 4; i++)
     {
         digitalWrite(s_pins[i], mux_channel[channel][i]);
     }
@@ -15,7 +40,7 @@ int hc4067_mux::read_channel_value(int channel)
 void hc4067_mux::setup()
 {
     pinMode(HC4067_MUX_DATA_PIN, INPUT);
-    for (size_t i; i < sizeof(s_pins); i++)
+    for (int i; i < 4; i++)
     {
         pinMode(s_pins[i], OUTPUT);
     }
--- src/hc4067_mux.h
+++ src/hc4067_mux.h
@@ -5,31 +5,6 @@
 
 namespace hc4067_mux
 {
-    const int s_pins[4] = {
-        HC4067_MUX_S0_PIN,
-        HC4067_MUX_S1_PIN,
-        HC4067_MUX_S2_PIN,
-        HC4067_MUX_S3_PIN};
-
-    const int mux_channel[16][4] = {
-        // {S0, S1, S2, S3,}
-        {0, 0, 0, 0},
-        {1, 0, 0, 0},
-        {0, 1, 0, 0},
-        {1, 1, 0, 0},
-        {0, 0, 1, 0},
-        {1, 0, 1, 0},
-        {0, 1, 1, 0},
-        {1, 1, 1, 0},
-        {0, 0, 0, 1},
-        {1, 0, 0, 1},
-        {0, 1, 0, 1},
-        {1, 1, 0, 1},
-        {0, 0, 1, 1},
-        {1, 0, 1, 1},
-        {0, 1, 1, 1},
-        {1, 1, 1, 1}};
-
     int read_channel_value(int channel);
     void setup();
 };
