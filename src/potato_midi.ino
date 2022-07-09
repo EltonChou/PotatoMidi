5a84b1639038f4e6993d1d46d91536fb66068812
Elton Chou <plscd748@gmail.com>
2022-07-10 04:14:25 +0800

    why this work?

--- src/potato_midi.ino
+++ src/potato_midi.ino
@@ -1,11 +1,15 @@
 #define MIDI_CHANNEL 1
 #define POLL_DELAY_MSEC 30
+#define get_midi_val(v) (constrain(v >> 3, 0, 127))
 
 #include <USB-MIDI.h>
 
+#include "hc4067_mux.h"
 #include "interface_controller.h"
 
 using namespace interface_controller;
+
+int gain_volumes[HC4067_MUX_CHANNEL_AMOUNT_TO_USE] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 USBMIDI_CREATE_INSTANCE(0, MIDI);
 
@@ -30,7 +34,25 @@ void setup()
 void loop()
 {
   MIDI.read();
-  gain_controller.refresh(MIDI, MIDI_CHANNEL);
+  // gain_controller.refresh(MIDI, MIDI_CHANNEL);
   btn_controller.refresh(MIDI, MIDI_CHANNEL);
+
+  for (uint8_t channel = 0; channel < HC4067_MUX_CHANNEL_AMOUNT_TO_USE; ++channel)
+  {
+    // Serial.print("CH: ");
+    // Serial.print(channel);
+    int a_val = hc4067_mux::read_channel_value(channel);
+    uint8_t strip_val = get_midi_val(a_val);
+    if (gain_volumes[channel] != strip_val)
+    {
+      gain_volumes[channel] = strip_val;
+      MIDI.sendControlChange(channel, strip_val, MIDI_CHANNEL);
+      // Serial.print("\tVol: ");
+      // Serial.print(strip_val);
+    }
+    // Serial.println();
+    // delay(200);
+  }
+
   delay(POLL_DELAY_MSEC);
 }
